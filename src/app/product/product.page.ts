f6fe6999aeaf6d984b472517b5385f6be5e84256
Alissa Jackson <arjack0815@gmail.com>
2024-05-14 07:30:45 -0600

    Product Page work

--- src/app/product/product.page.ts
+++ src/app/product/product.page.ts
@@ -2,14 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonItemDivider, IonButton } from '@ionic/angular/standalone';
+import { single } from 'rxjs';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.page.html',
   styleUrls: ['./product.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardHeader, IonCardTitle, IonCardContent]
+  imports: [IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonItemDivider]
 })
 export class ProductPage implements OnInit {
 
@@ -17,6 +18,9 @@ export class ProductPage implements OnInit {
   constructor(private http: HttpClient) { }
 
   singleProduct: any;
+  addToCart() {
+    console.log(this.singleProduct + ' Product added to cart');
+  }
   ngOnInit() {
     this.http.get<any>('http://makeup-api.herokuapp.com/api/v1/products/1048.json')
       .subscribe((data) => {
